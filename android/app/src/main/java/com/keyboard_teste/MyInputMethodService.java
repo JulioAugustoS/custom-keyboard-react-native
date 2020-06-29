ce842b81875e8cba9ba82881de6c43e269af41e2
Julio Augusto <julioams@icloud.com>
2020-06-29 16:47:26 -0300

    teste for keyboard

--- android/app/src/main/java/com/keyboard_teste/MyInputMethodService.java
+++ android/app/src/main/java/com/keyboard_teste/MyInputMethodService.java
@@ -1,19 +1,82 @@
 package com.keyboard_teste;
 
+import android.graphics.Color;
 import android.inputmethodservice.InputMethodService;
+import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
-public class MyInputMethodService extends InputMethodService {
-    private RNKeyboardView rnKeyboardView;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
 
-    public MyInputMethodService() {
-        super();
-        rnKeyboardView = new RNKeyboardView();
-    }
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+public class MyInputMethodService extends InputMethodService implements DefaultHardwareBackBtnHandler {
+    private @Nullable ReactInstanceManager mReactInstanceManager;
+    private @Nullable ReactRootView mReactRootView;
+
+    private LifecycleState mLifecycleState = LifecycleState.BEFORE_RESUME;
+
+    private View v;
 
     @Override
     public View onCreateInputView() {
-        return rnKeyboardView.createCustomKeyboardView();
+        android.os.Debug.waitForDebugger();
+        try {
+            v = getLayoutInflater().inflate(R.layout.keyboard_view, null);
+            v.setBackgroundColor(Color.BLACK);
+
+            mReactInstanceManager = ReactInstanceManager.builder()
+                    .setApplication(getApplication())
+                    .setBundleAssetName("index.android.bundle")
+                    .setJSMainModulePath("index")
+                    .addPackage(new MainReactPackage())
+                    .setUseDeveloperSupport(BuildConfig.DEBUG)
+                    .setInitialLifecycleState(LifecycleState.RESUMED)
+                    .build();
+
+            mReactRootView = createRootView();
+            mReactRootView.startReactApplication(mReactInstanceManager, getMainComponentName(), getLaunchOptions());
+
+            ViewGroup insertPoint = (ViewGroup) v.findViewById(R.id.react_test_id);
+            insertPoint.addView(mReactRootView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+            v.invalidate();
+        } catch (Exception e) {
+            int z = 1;
+            int y = 2;
+        }
+
+        return v;
     }
 
+    protected @Nullable Bundle getLaunchOptions() {
+        Bundle b = new Bundle();
+        b.putString("mode", "keyboard");
+
+        return b;
+    }
+
+    protected ReactRootView createRootView() {
+        return new ReactRootView(this);
+    }
+
+    protected String getMainComponentName() {
+        return "RNCustomKeyboard";
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+
+    }
 }
--- android/app/src/main/java/com/keyboard_teste/RNKeyboardView.java
+++ android/app/src/main/java/com/keyboard_teste/RNKeyboardView.java
@@ -2,38 +2,51 @@ package com.keyboard_teste;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactRootView;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.uimanager.ReactRoot;
 
 public class RNKeyboardView extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public RNKeyboardView(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
     @NonNull
     @Override
     public String getName() {
-        return "RNCustomKeyboard";
+        return "CustomKeyboardView";
     }
 
     ReactRootView rootView = null;
 
-    public RelativeLayout createCustomKeyboardView() {
-        final Activity activity = getCurrentActivity();
-
+    public View createCustomKeyboardView(Activity activity) {
         RelativeLayout layout = new RelativeLayout(activity);
         rootView = new ReactRootView(this.getReactApplicationContext());
-        rootView.setBackgroundColor(Color.GREEN);
+//        rootView.setBackground(Color.BLACK);
 
-//        Bundle bundle = new Bundle();
+        Bundle bundle = new Bundle();
+        rootView.startReactApplication(
+                ((ReactApplication) activity.getApplication()).getReactNativeHost().getReactInstanceManager(),
+                "CustomKeyboardView",
+                bundle);
 
         final float scale = activity.getResources().getDisplayMetrics().density;
         RelativeLayout.LayoutParams lParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Math.round(216*scale));
-        lParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
         layout.addView(rootView, lParams);
 
         return layout;
     }
-
 }
